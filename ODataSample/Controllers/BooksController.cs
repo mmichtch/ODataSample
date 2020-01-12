ee82c244a226d1180d4c8bf9c6a189a3edfc3802
Maxim Michtchenko <Maxim@4refuel.com>
2020-01-11 20:07:02 -0800

    Added books controller

---
+++ ODataSample/Controllers/BooksController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Routing;
+using Microsoft.AspNetCore.Mvc;
+using ODataSample.Data;
+using ODataSample.Model;
+
+namespace ODataSample.Controllers
+{
+    [ODataRoutePrefix("Books")]
+    public class BooksController : ODataController
+    {
+        private readonly LibraryContext db;
+
+        public BooksController(LibraryContext dbContext)
+        {
+            this.db = dbContext;
+        }
+
+        [HttpGet]
+        [EnableQuery]
+        public IQueryable<Book> Get()
+        {
+            return db.Books.AsQueryable();
+        }
+
+        [HttpGet]
+        [EnableQuery]
+        public SingleResult<Book> Get(int key)
+        {
+            return SingleResult.Create(db.Books.Where(c => c.Id == key));
+        }
+    }
+}
